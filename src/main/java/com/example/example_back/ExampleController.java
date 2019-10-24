8e01553cfd726c01f42459b23c2c6cb013765295
taiberium <taiberium@mail.ru>
2019-10-24 22:59:39 +0300

    add new version

--- src/main/java/com/example/example_back/ExampleController.java
+++ src/main/java/com/example/example_back/ExampleController.java
@@ -4,16 +4,38 @@ import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 public class ExampleController {
 
     @GetMapping("version")
     public VersionDto getVersion() {
-        return new VersionDto("1");
+        return new VersionDto("2");
+    }
+
+    @GetMapping("info")
+    public ServerInfoDto getServerInfo() {
+        LocalDateTime uptime = LocalDateTime.now().minusYears(2019).minusDays(LocalDate.now().getDayOfYear());
+        return new ServerInfoDto(uptime, getRandomPercentage(), getRandomPercentage(), getRandomPercentage());
+    }
+
+    private int getRandomPercentage() {
+        return ThreadLocalRandom.current().nextInt(100);
     }
 
     @Value
     private static class VersionDto {
         private String version;
+    }
+
+    @Value
+    private static class ServerInfoDto {
+        private LocalDateTime uptime;
+        private int ram;
+        private int proc;
+        private int hard;
     }
 }
